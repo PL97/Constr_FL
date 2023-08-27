fix some normalization bugs

NP/NPclf.py
@@ -10,13 +10,27 @@ for ii in range(1, num_seed + 1):
     print(ii)
     np.random.seed(ii)
 
-    d = 10 #! 30 ~ 50
-    n = 5
-    Ni0 = 1000 #! 30000
-    Ni1 = 300
+    d = 3 #! 30 ~ 50
+    n = 2
+    Ni0 = 300 #! 30000
+    Ni1 = 100
 
     X0 = (np.random.rand(d, Ni0, n) - 0.5) * 0.1
     X1 = (np.random.rand(d, Ni1, n) + 0.5)*0.1
+    
+    # for i in range(n):
+    #     X0[-1, :, i] = 1
+    #     X1[-1, :, i] = 1    
+    
+    
+    X1full = np.zeros((d, Ni1 * n))
+    for i in range(n):
+        X1full[:, i * Ni1:(i+1) * Ni1] = X1[:, :, i]
+        
+    X0full = np.zeros((d, Ni0 * n))
+    for i in range(n):
+        X0full[:, i * Ni0:(i+1) * Ni0] = X0[:, :, i]
+    
 
     X1full = np.zeros((d, Ni1 * n))
     for i in range(n):
@@ -29,8 +43,19 @@ for ii in range(1, num_seed + 1):
     eps = 1e-3
     wfeas = minimize(Loss1, w0, tol=eps, method="L-BFGS-B")['x']
     
+    
+    import matplotlib.pyplot as plt
+    w = wfeas
+    plt.scatter(X0full[0, :], X0full[1, :], label="0")
+    plt.scatter(X1full[0, :], X1full[1, :], label="1")
+    plt.legend()
+    x = np.array(range(-1, 1))
+    y = -(w[0]/w[1]) * x - w[2]/w[1]
+    plt.plot(x, y)
+    plt.savefig("data.png")
+    
     rfull = np.zeros(n)
-    delta_r = 1 * Ni1
+    delta_r = 10 * Ni1
     for i in range(n):
         # rfull[i] = np.sum(-np.log(sigmoid(wfeas.T @ X1[:, :, i]))) + delta_r
         rfull[i] = delta_r
@@ -38,7 +63,7 @@ for ii in range(1, num_seed + 1):
     w0 = np.zeros(d)
     mu0full = np.zeros(n)
     rhofull = np.ones(n) * 0.01 #! set a smaller number, check the constraints
-    beta = 1
+    beta = 10
     eps1 = 1e-3
     eps2 = 1e-3
 
@@ -56,3 +81,14 @@ print("Average In Iterations:", ave_in_iter)
 print("Average Stat Val:", ave_stat_val)
 print("Average Feas Val:", ave_feas_val)
 print("Average Obj Val:", ave_obj_val)
+
+
+import matplotlib.pyplot as plt
+w = wfeas
+plt.scatter(X0full[0, :], X0full[1, :], label="0")
+plt.scatter(X1full[0, :], X1full[1, :], label="1")
+plt.legend()
+x = np.array(range(-1, 1))
+y = (w[1]/w[0]) * x - w[2]
+plt.plot(x, y)
+plt.savefig("data_final.png")

NP/NPclf_UCI.py
@@ -35,7 +35,7 @@ for ii in range(1, num_seed + 1):
     
     
     rfull = np.zeros(n)
-    delta_r = 5 * Ni1
+    delta_r = 0.1 * Ni1
 
     for i in range(n):
         # rfull[i] = np.sum(-np.log(sigmoid(wfeas.T @ X1[:, :, i]))) + delta_r
@@ -43,8 +43,8 @@ for ii in range(1, num_seed + 1):
 
     w0 = np.zeros(d)
     mu0full = np.zeros(n)
-    rhofull = np.ones(n) * 0.1 #! set a smaller number, check the constraints
-    beta = 1
+    rhofull = np.ones(n) * 0.01 #! set a smaller number, check the constraints
+    beta = 10
     eps1 = 1e-3
     eps2 = 1e-3
 

NP/acc_loss.py
@@ -44,9 +44,9 @@ def DLoss2(w):
 
 
 w0 = np.ones(d)
-lr = 1
+lr = 0.001
 
-total_e = 50
+total_e = 50000
 
 for i in range(total_e):
     print(Loss2(w0))
@@ -59,15 +59,15 @@ for i in range(total_e):
     
 print("------------------------------------")
 
-lr = 10
+# total_e = 5000
+# lr = 0.1
 
-for i in range(total_e):
-    print(Loss1(w0))
-    w0 -= lr * DLoss1(w0)
-    model_eval(w0, X0full_ex, X1full_ex)
+# for i in range(total_e):
+#     print(Loss1(w0))
+#     w0 -= lr * DLoss1(w0)
+#     model_eval(w0, X0full_ex, X1full_ex)
     
     
-w0 = minimize(Loss1, w0, tol=0.0001, method="L-BFGS-B")['x']
-model_eval(np.ones(d), X0full_ex, X1full_ex)
-asdf
+# w0 = minimize(Loss1, w0, tol=0.0001, method="L-BFGS-B")['x']
+# model_eval(np.ones(d), X0full_ex, X1full_ex)
     

NP/admm.py
@@ -14,7 +14,7 @@ def sigmoid(x):
 def admm(w0, X0, X1, mu, r, rhofull, beta, tau):
     d, _, n = X0.shape
     # q = 0.9
-    q = 0.9
+    q = 0.95
 
     u0 = np.zeros((d, n))
     for i in range(n):
@@ -23,11 +23,13 @@ def admm(w0, X0, X1, mu, r, rhofull, beta, tau):
     lmda = np.zeros((d, n))
     for i in range(n):
         def DPi(w):
-            sigmoid_term = sigmoid(np.dot(w.T, X0[:, :, i]))
-            first_term = np.dot(X0[:, :, i], sigmoid_term.T)
-            second_term = -np.dot(X1[:, :, i], 1 / (1 + np.exp(np.dot(w.T, X1[:, :, i]))))
+            # sigmoid_term = sigmoid(np.dot(w.T, X0[:, :, i]))
+            inner_prod_X0 = np.dot(w.T, X0[:, :, i])
+            inner_prod_X1 = np.dot(w.T, X1[:, :, i])
+            first_term = np.dot(X0[:, :, i], sigmoid(inner_prod_X0).T)
+            second_term = -np.dot(X1[:, :, i], sigmoid(-inner_prod_X1))
             third_term = (w - w0) / ((n + 1) * beta)
-            return (1/(X0.shape[1]))*(first_term) + (1/X1.shape[1]) * (second_term*np.maximum(mu[i] + beta * (np.sum(-np.dot(w.T, X1[:, :, i]) + np.log(1 + np.exp(np.dot(w.T, X1[:, :, i])))) - r[i]), 0)) + third_term
+            return (1/(X0.shape[1]))*(first_term) + (1/X1.shape[1]) * (second_term*np.maximum(mu[i] + beta * (np.sum(-np.log(sigmoid(inner_prod_X1))) - r[i]), 0)) + third_term
         
         
         

NP/proxAL.py
@@ -58,8 +58,8 @@ def proxAL(w0, mu0full, X0, X1, r, admm, beta, rhofull, eps1, eps2):
             obj_mea[i] = np.sum(inner_prod_X0-np.log(sigmoid(inner_prod_X0)+eps)) 
         
         feas_val = max(feas_mea)
-        print(feas_mea)
-        print(obj_mea)
+        # print(feas_mea)
+        # print(obj_mea)
         print(model_eval(wc, X0, X1))
 
         if np.linalg.norm(wp - wc, np.inf) + beta * tauk <= beta * eps1:

NP/utils/eval.py
@@ -5,8 +5,11 @@ def model_eval(w, X0, X1):
     ret = []
     for i in range(X1.shape[-1]):
         TN = np.sum(sigmoid(w.T @ X0[:, :, i]) < 0.5)
-        TP = np.sum(sigmoid(w.T @ X1[:, :, i]) > 0.5)
+        TP = np.sum(sigmoid(w.T @ X1[:, :, i]) >= 0.5)
         ret.append({"Precall": TP/X1.shape[1]})
         ret.append({"Nrecall": TN/X0.shape[1]})
     print(ret)
+    # print((w.T @ X1[:, :, i])[:10])
+    # print((w.T @ X0[:, :, i])[:10])
+    # print(TN, TP)
     return ret